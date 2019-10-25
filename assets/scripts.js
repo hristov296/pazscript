60deb6f8570720bc2b5edfb5e4e05633977be08e
hristov296 <hristov296@gmail.com>
2019-10-25 17:35:15 +0300

    add some scripts

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,37 +1,62 @@
 (function($) {
   $(document).ready(function() {
+    $items = [];
+    active = -1;
     $(".show-chart").each(function(i, el) {
+      $items.push(el);
       $(this).click(function() {
-        $(".main-list li a").removeClass("active");
-        $(this)
-          .siblings("a")
-          .addClass("active");
-        b = [plots[i].ploty1, plots[i].ploty2];
-        c = plots[i].plotx;
-        $("#canvas").plot(b, {
-          xaxis: {
-            ticks: c,
-          },
-          grid: {
-            hoverable: true,
-            clickable: true,
-            mouseActiveRadius: 20,
-            autoHighlight: true,
-          },
-          crosshair: {
-            mode: "x",
-            color: "#fff",
-          },
-          series: {
-            lines: {
-              show: true,
-            },
-            points: {
-              show: true,
-            },
-          },
-        });
+        changeActive(i);
       });
+    });
+
+    function changeActive(i) {
+      if (i < 0 || i > $items.length - 1) {
+        return;
+      }
+      $(".main-list li a").removeClass("active");
+      $($items[i]).addClass("active");
+      $($items[i])
+        .siblings("a")
+        .addClass("active");
+
+      b = [plots[i].ploty1, plots[i].ploty2];
+      c = plots[i].plotx;
+      $("#canvas").plot(b, {
+        xaxis: {
+          ticks: c,
+        },
+        grid: {
+          hoverable: true,
+          clickable: true,
+          mouseActiveRadius: 20,
+          autoHighlight: true,
+        },
+        crosshair: {
+          mode: "x",
+          color: "#fff",
+        },
+        series: {
+          lines: {
+            show: true,
+          },
+          points: {
+            show: true,
+          },
+        },
+      });
+
+      $($items[i]).focus();
+      active = i;
+    }
+
+    $(window).keydown(function(e) {
+      if (e.keyCode == 39 || e.keyCode == 40) {
+        e.preventDefault();
+        changeActive(active + 1);
+      } else if (e.keyCode == 37 || e.keyCode == 38) {
+        e.preventDefault();
+        changeActive(active - 1);
+      }
     });
   });
 })(jQuery);
